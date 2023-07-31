ffbdc399a115b4961925a5ab75207a1d9ae87cf6
soulunknown <131215509+soulunknown@users.noreply.github.com>
2023-07-31 17:54:56 -0500

    Select a Featured Puppy

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-import {puppyList} from './data.js'
-import { useState } from 'react'
-console.log(puppyList)
-import './App.css'
+import { puppyList } from "./data.js";
+import { useState } from "react";
+console.log(puppyList);
+import "./App.css";
 function App() {
-
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   console.log("puppyList: ", puppyList);
+  console.log(featuredPup);
 
   return (
     <div className="App">
-      {
-        puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+      {featPupId && (
+
+      <div>
+        <h2>{featuredPup.name}</h2>
+        <ul>
+          <li>Age: {featuredPup.age}</li>
+          <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
